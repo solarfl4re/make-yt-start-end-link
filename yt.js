05846402f81d8b1a5098910251879b23b7dfc7c8
David <solarfl4re@users.noreply.github.com>
2021-03-06 18:53:33 -0900

    Finish yt URL parsing & loading

--- yt.js
+++ yt.js
@@ -1,16 +1,19 @@
-	let ytURL = '';
-	let player;
+function setup() {
+  let ytURL = '';
+  let player;
 
-// Example URLs
-let ytUrl1 = 'https://www.youtube.com/watch?v=MnynrNNVDy8';
-let ytUrl2 = 'https://youtu.be/MnynrNNVDy8';
-	
-	// load YT API js
-	    let tag = document.createElement('script');
+  // Example URLs
 
-      tag.src = "https://www.youtube.com/iframe_api";
-      let firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  // load YT API js
+  let tag = document.createElement('script');
+
+  tag.src = "https://www.youtube.com/iframe_api";
+  let firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  let ytForm = document.getElementById('ytUrlForm');
+  ytForm.addEventListener('submit', loadVideo);
+}
 	
       function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
@@ -20,32 +23,41 @@ let ytUrl2 = 'https://youtu.be/MnynrNNVDy8';
 					);
       }
 
-const form = document.getElementById('ytUrlForm');
-form.addEventListener('submit', loadVideo);
 
   function getYtId(url) {
-    let starts = ['v=', 'youtu.be/']
-    let ends = ['&', '?']
+    if (url.indexOf('v=') !== -1) {
+      let start = url.indexOf('v=') + 2;
+      let end = url.indexOf('&');
+      
+      return url.substring(start, end !== -1 ? end : undefined)
 
-    //TODO: use 'for'
-    for (let i = 0; found == true; i++)
-    starts.forEach((start, i) => {
-      let startPos = url.indexOf(start);
-      if startPos !== -1 {
-        let endPos = url.indexOf(ends[i]);
-        let id = url.slice(
-          startPos + start.length, endPos !== -1 ? endPos : '');
+    } else if (url.indexOf('youtu.be/') !== -1) {
+      let start = url.indexOf('youtu.be/') + 9;
+      let end = url.indexOf('?');
 
+      return url.substring(start, end !== -1 ? end : undefined)
 
-
+    } else {
+      // take whole string as video id
+      return url;
+  }
   }
 	
 	function loadVideo(event) {
 // load video w/ yt api here
-		alert(ytURL +"!")
+    let ytInput = document.getElementById('ytUrlInput').value
+    let ytId = getYtId(ytInput)
+    player.loadVideoById(ytId)
     event.preventDefault();
 	}
 
-// Test YT url parse
-console.log(getYtId(ytUrl1));
-console.log(getYtId(ytUrl2));
+function testGetYtUrl() {
+  let ytUrl1 = 'https://www.youtube.com/watch?v=MnynrNNVDy8';
+  let ytUrl2 = 'https://youtu.be/MnynrNNVDy8';
+
+  // Test YT url parse
+  console.log(getYtId(ytUrl1));
+  console.log(getYtId(ytUrl2));
+}
+
+window.addEventListener('load', setup);
